32c2ef05a1b747bb18999d5f710bec6c99674eb1
Yona <uzicandi@gmail.com>
2021-03-11 01:31:23 +0900

    removeFrom

--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -4,8 +4,10 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void;
 class PageItemComponent extends BaseComponent<HTMLElement>
   implements Composable {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
     <section class="page-item__body"></section>
@@ -13,12 +15,19 @@ class PageItemComponent extends BaseComponent<HTMLElement>
       <button class="close">&times;</button>
     </div>
   </li>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector(
       '.page-item__body'
     )! as HTMLElement;
     child.attachTo(container);
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -32,5 +41,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement>
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
