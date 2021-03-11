24081caf107cef4f2a77afe19fc09be826ad6070
Yona <uzicandi@gmail.com>
2021-03-11 14:55:09 +0900

    dialog

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -1,3 +1,4 @@
+import { InputDialog } from './components/dialog/dialog.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
@@ -15,6 +16,17 @@ class App {
         this.page.addChild(note);
         const todo = new TodoComponent('Todo Title', 'Todo Item');
         this.page.addChild(todo);
+        const imageBtn = document.querySelector('#new-image');
+        imageBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+            dialog.setOnCloseListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.setOnSubmitListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.attachTo(document.body);
+        });
     }
 }
 new App(document.querySelector('.document'));
--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,4 +1,5 @@
-import { Component } from './components/component';
+import { InputDialog } from './components/dialog/dialog.js';
+import { Component } from './components/component.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
@@ -32,6 +33,19 @@ class App {
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+    imageBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 페이지에 추가해준다.
+        dialog.removeFrom(document.body);
+      });
+      dialog.attachTo(document.body);
+    });
   }
 }
 
